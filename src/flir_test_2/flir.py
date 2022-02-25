7c9e3a82f792e62a2fcc0186975382e1b6d6baa5
Douglas Thor <doug.thor@gmail.com>
2022-02-25 14:45:50 -0800

    Add logging and a stub for splitting out clusters

--- src/flir_test_2/flir.py
+++ src/flir_test_2/flir.py
@@ -4,10 +4,13 @@ FLIR Python Evaluation, test 2
 """
 from pathlib import Path
 from typing import Any
+from typing import List
 from typing import Tuple
 
 import imageio
 import numpy as np
+
+from loguru import logger
 
 from flir_test_2.custom_types import CoordType
 
@@ -22,6 +25,38 @@ def out_of_bounds(array: np.ndarray, coord: CoordType) -> bool:
         row_coord < 0 or col_coord < 0 or row_coord >= num_rows or col_coord >= num_cols
     )
     return is_out_of_bounds
+
+
+def split_clusters(cluster_data: List[CoordType]) -> List[List[CoordType]]:
+    """
+    Split out a single list of coords representing N clusters into N lists.
+
+    The input ``cluster_data`` is a list of ``(row, col)`` tuples. For each point
+    in the input data, we look for any other coord that is neighboring that
+    coord. If no neighbors are found, that point is removed and set aside in
+    a separate list.
+    """
+    logger.info("Splitting out clusters.")
+
+    # WIP
+    return [
+        [
+            (0, 0),
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (3, 4),
+            (4, 3),
+            (4, 4),
+            (5, 2),
+            (5, 5),
+            (6, 1),
+            (6, 6),
+            (7, 0),
+            (7, 7),
+        ],
+        [(0, 7), (1, 6)],
+    ]
 
 
 def load_image_file(path: Path) -> np.ndarray:
