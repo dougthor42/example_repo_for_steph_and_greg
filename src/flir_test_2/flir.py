5e11e48f49283c3a9bb5651aa2337c1c6a29853f
Douglas Thor <doug.thor@gmail.com>
2022-02-25 10:46:31 -0800

    Initial commit of flir.py

---
+++ src/flir_test_2/flir.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+FLIR Python Evaluation, test 2
+"""
+from pathlib import Path
+from typing import Any
+from typing import Tuple
+
+import imageio
+import numpy as np
+
+
+def load_image_file(path: Path) -> np.ndarray:
+    pass
+
+
+# This function name, arg name, and return values are defined by the program requirements.
+def find_marked_pixels(image_array: np.ndarray) -> Tuple[int, Any, Any]:
+    pass
+
+
+# This function name, arg name, and return values are defined by the program requirements.
+def encode_pixel_map(image_array):
+    pass
+
+
+def example():
+    pass
+
+
+if __name__ == "__main__":
+    example()
