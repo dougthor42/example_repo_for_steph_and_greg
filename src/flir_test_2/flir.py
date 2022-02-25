2d9092a4de9dc907043c03e98c6bf59437e870ae
Douglas Thor <doug.thor@gmail.com>
2022-02-25 14:47:57 -0800

    Add flir.is_neighbor

--- src/flir_test_2/flir.py
+++ src/flir_test_2/flir.py
@@ -9,7 +9,6 @@ from typing import Tuple
 
 import imageio
 import numpy as np
-
 from loguru import logger
 
 from flir_test_2.custom_types import CoordType
@@ -25,6 +24,50 @@ def out_of_bounds(array: np.ndarray, coord: CoordType) -> bool:
         row_coord < 0 or col_coord < 0 or row_coord >= num_rows or col_coord >= num_cols
     )
     return is_out_of_bounds
+
+
+def is_neighbor(coord: CoordType, other: CoordType) -> bool:
+    """
+    Return True if ``other`` is a neighbor of ``coord``.
+
+    ``coord`` and ``other`` are tuples of (row, column) integers.
+
+    A neighbor is any coordinate that is touching the given coordinate.
+
+    ::
+       Given coordinate: "+"
+       Neighbors: "X"
+       Not Neighbors: "."
+
+       .......
+       ..XXX..
+       ..X+X..
+       ..XXX..
+       .......
+
+    A coordinate **is** considered a neighbor of itself.
+
+    Parameters
+    ----------
+    coord
+        The reference coordinate
+    other
+        The coordinate to check if it's a neighbor of ``coord``.
+
+    Returns
+    -------
+    bool
+
+    Examples
+    --------
+    >>> is_neighbor((0, 0), (0, 0))
+    True
+    >>> is_neighbor((0, 0), (1, 1))
+    True
+    >>> is_neighbor((1, 1), (1, 3))
+    False
+    """
+    return abs(coord[0] - other[0]) <= 1 and abs(coord[1] - other[1]) <= 1
 
 
 def split_clusters(cluster_data: List[CoordType]) -> List[List[CoordType]]:
