61ca372d10d762bd9d5665a0c4b1cec65027b28e
Douglas Thor <doug.thor@gmail.com>
2022-02-25 14:39:36 -0800

    Add flir.out_of_bounds

--- src/flir_test_2/flir.py
+++ src/flir_test_2/flir.py
@@ -9,6 +9,20 @@ from typing import Tuple
 import imageio
 import numpy as np
 
+from flir_test_2.custom_types import CoordType
+
+
+def out_of_bounds(array: np.ndarray, coord: CoordType) -> bool:
+    """
+    Check if a coordinate is out of bounds of an array.
+    """
+    row_coord, col_coord = coord
+    num_rows, num_cols = array.shape
+    is_out_of_bounds = (
+        row_coord < 0 or col_coord < 0 or row_coord >= num_rows or col_coord >= num_cols
+    )
+    return is_out_of_bounds
+
 
 def load_image_file(path: Path) -> np.ndarray:
     """
